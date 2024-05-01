0938be994fbbe9b45605cc5cae7150144fcc739d
Mohamed Machlou <medmachlou@gmail.com>
2024-05-01 09:00:06 +0100

    create components

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { CreateCategorieComponent } from './components/create-categorie/create-categorie.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    CreateProductComponent,
+    CreateCategorieComponent,
+    EditProductComponent,
+    LoginComponent,
+    LogoutComponent,
+    PageNotFoundComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/client/components/create-categorie/create-categorie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-categorie',
+  templateUrl: './create-categorie.component.html',
+  styleUrl: './create-categorie.component.css'
+})
+export class CreateCategorieComponent {
+
+}
